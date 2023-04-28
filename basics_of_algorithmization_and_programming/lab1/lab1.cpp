ae6eb18769168d4e4f7424a7babfa52f4cb58691
Gerodote <lg3dx6fd@gmail.com>
2023-04-29 00:51:48 +0300

    added part3

--- basics_of_algorithmization_and_programming/lab1/lab1.cpp
+++ basics_of_algorithmization_and_programming/lab1/lab1.cpp
@@ -2,8 +2,10 @@
 #include "calculate.hpp"
 #include <iomanip>
 #include <iostream>
+#include <cmath>
 
 int main() {
+{
     double y, a, b;
     std::cout << "Enter the values of y, a, and b: ";
     std::cin >> y >> a >> b;
@@ -11,7 +13,8 @@ int main() {
     double result = calc(y, a, b);
     std::cout << std::fixed << std::setw(8) << std::setprecision(4) << result
               << std::endl;
-
+}
+ {
     double radius, side1, side2, angle;
     std::cout << "Enter the radius of the circle: ";
     std::cin >> radius;
@@ -29,6 +32,12 @@ int main() {
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
-
+ }
+{
+    const double a = 1.1;
+    const double b = 0.3;
+    double a_stupid_calc = std::pow(std::sin(b),3) + a*std::log10(a-std::exp(b)) - a/std::log(a-std::exp(b));
+    std::cout << "result of part 3 task: " << a_stupid_calc << "\n if you got nan -- that\'s right. it's because there's a negative value of value under logarithm. Such stuff is not defined for logarithm, so the task is strange." << std::endl;
+}
     return 0;
 }
