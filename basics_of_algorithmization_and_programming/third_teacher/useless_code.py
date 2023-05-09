8718f2170c39641cb1bec26e839348d4ec19f80d
Gerodote <lg3dx6fd@gmail.com>
2023-05-09 17:30:48 +0300

    adding a useless code

---
+++ basics_of_algorithmization_and_programming/third_teacher/useless_code.py
@@ -0,0 +1,10 @@
+
+def foo(something:str):
+    print(something)
+
+class Bar():
+    def __init__(self, something:str):
+        self.something = something
+
+    def print(self):
+        print(self.something)
